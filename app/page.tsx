ef21849d97cec2a9d4334caab0c87e22e0b1ded5
wilo1819lucero-prog <wilo1819lucero@gmail.com>
2026-05-03 21:38:19 -0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-green-600">
+      <h1 className="text-4xl font-bold text-white">Liga Sayausí funcionando ✅</h1>
+    </main>
+  );
+}
